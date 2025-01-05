b6de0a723c98d73d89894eba6a5cb8a9bfd62a5c
Ding Zhen <69374148+localhosts-A@users.noreply.github.com>
2025-01-05 05:33:50 +0000

    build: simplify ZIP filename in build.sh and remove buildall.sh script

--- build.sh
+++ build.sh
@@ -226,7 +226,7 @@ echo "Build for MIUI finished."
 
 cd anykernel 
 
-ZIP_FILENAME=Kernel_MIUI_${TARGET_DEVICE}_${KSU_ZIP_STR}_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
+ZIP_FILENAME=Kernel_MIUI_${TARGET_DEVICE}_anykernel3.zip
 
 zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
 
